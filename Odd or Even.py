39a59423515aaab99394b0e2c69072813a27468e
keerths7 <rkeers1111@gmail.com>
2024-09-20 12:31:43 +0530

    Solved 1 ques on day 1

--- Odd or Even.py
+++ Odd or Even.py
@@ -1,5 +1,21 @@
 '''
 Problem : https://www.geeksforgeeks.org/problems/odd-or-even3618/1
 '''
+#Solution 1 
+def evenodd(n):
+    if n % 2 == 0:
+        return "even"
+    return "odd"
 
-#Solution 1
+#Solution 2
+def evenodd2(n):
+    return "even" if n % 2 == 0 else "odd"
+
+#Solution 3 - fastest
+def evenodd3(n):
+    if n & 1 == 1:
+        return "odd"
+    return "even"
+
+n = int(input("Enter the number:"))
+print(evenodd3(n))
