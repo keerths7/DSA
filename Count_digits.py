b4321ec1768242677e1ff56e8d73163e7dc5c9e3
keerths7 <rkeers1111@gmail.com>
2024-09-28 12:36:06 +0530

    Made some modifications

--- Count_digits.py
+++ Count_digits.py
@@ -1,18 +1,20 @@
 '''
 Problem : Count the number of digits in a number
 '''
-# Solution 1- 
+# Solution 1- Using string conversion
+
 def count_digits_stri(n):
     return len(str(n))
 
-# Solution 2-
-def count_digits_remainder(n):
+# Solution 2- Using division
+
+def count_digits_div(n):
     count = 0
-    while(n % 10 < 0):
+    while n > 0:
+        n = n // 10 
         count += 1
     return count
 
-
 n = int(input("Enter the number:"))
-#print(count_digits_stri(n))
-print(count_digits_remainder(n))
+print(count_digits_stri(n))
+print(count_digits_div(n))
--- Find_last_digit.py
+++ Find_last_digit.py
@@ -1,9 +1,10 @@
 '''
 Problem : Finding the last digit in a number
 '''
-# Solution 1- using string
+# Solution 1- using string conversion
 # Time Complexity: O(k) - where k is the number of digits in the number which turns out to be O(logn)
 # Space Complexity: O(k) - where k is the number of digits in the number which turns out to be O(logn)
+
 def last_digit_stri(n):
     str_n = str(n)
     return str_n[-1]
@@ -11,6 +12,7 @@ def last_digit_stri(n):
 # Solution 2- using remainder 
 # Time Complexity: O(1)
 # Space Complexity: O(1)- no extra space used
+
 def last_digit_remainder(n):
     return n % 10
 
