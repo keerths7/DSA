308538128055d73869c9152c554ff77022ddf5b5
keerths7 <rkeers1111@gmail.com>
2024-11-21 19:41:32 +0530

    some changes

---
+++ LearningDSA/Lists/sliceDelete.py
@@ -0,0 +1,7 @@
+myList = ['a', 'b', 'c', 'd', 'e', 'f']
+
+# lets say we want to print the first two elements of the list, we can do it using slice operator
+
+print(myList[:2])
+print(myList[1:])
+print(myList[:])
--- basics/Power_reverse_num.py
+++ basics/Power_reverse_num.py
@@ -2,7 +2,7 @@
 Problem: https://www.geeksforgeeks.org/problems/count-digits5716/1
 '''
 # Solution 1- Using logic
-# Time Complexity: O(log(reversed(n)))- The number of digits k is proportional to log10(n), so we can say the time complexity is O(log n).
+# Time Complexity: O(log(reversed(n)))
 # Space Complexity: O(1)- no extra space used
 
 def reversed_num_powered(num):
@@ -11,7 +11,7 @@ def reversed_num_powered(num):
     while num_var > 0:  # O(logn) time complexity
         reversed_num = reversed_num * 10 + num_var % 10 
         num_var = num_var // 10
-    return num ** reversed_num  # O(n) time complexity
+    return num ** reversed_num  # O(log(reversed(n)) time complexity
 
 
 num = int(input("Enter the number: "))
