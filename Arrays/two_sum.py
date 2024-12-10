49ba98e12b18824c8924ea7711799cc98207162e
rkeers1111@gmail.com <rkeers1111@gmail.com>
2024-12-10 22:16:04 +0530

    added some code

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -13,36 +13,37 @@ def find_pairs_two_sum(nums, target):
                 return [i,j]
 
 
-# Solution 2- Using Brute Force with logic
+# Solution 2- Using logic with lists
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
 
 def find_pairs_two_sum_logic(nums,target):
-    for i in nums:
+    for i in nums:      # O(n)
         x = target - i
-        if x in nums:
-            return [nums.index(i), nums.index(x)]
+        if x in nums:    # O(n) for in operator used with lists                    
+            return [nums.index(i), nums.index(x)]   # O(n) + O(n) = O(2n) to calculate index
 
 
-# Solution 2- Using Brute Force with logic
+# Solution 3 - Using logic with lists and enum
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
 
 def find_pairs_two_sum_logic_enum(nums,target):
-    for i, value in enumerate(nums):
+    for i, value in enumerate(nums): # O(n)
         x = target - value
-        if x in nums:
-            return [i, nums.index(x)]
+        if x in nums:       # O(n) for in operator used with lists
+            return [i, nums.index(x)]   # O(n) to calculate index
         
-# Solution 2- Using Brute Force with logic
-# Time Complexity: O(n^2)
+
+# Solution 4- Using logic with dict
+# Time Complexity: O(n)
 # Space Complexity: O(1)
   
 def find_pairs_two_sum_dict(nums,target):
     seen = {}
-    for i, value in enumerate(nums):
+    for i, value in enumerate(nums):    # O(n)
         x = target - value
-        if x in seen:
+        if x in seen:    # O(1) for in operator used with dict
             return [seen[x], i]
         seen[value] = i
         
