d918b10ea28a6e38de47d0ab99b70844328b331b
rkeers1111@gmail.com <rkeers1111@gmail.com>
2025-01-20 21:37:07 +0530

    made few changes

--- basics/Nth_Armstrong.py
+++ basics/Nth_Armstrong.py
@@ -1,27 +1,51 @@
-def nth_armstrong(nth):
+# def nth_armstrong(nth):
+#     count = 0
+#     num = 1
+
+#     while count != nth:
+#         num_count = num
+#         num_digit = num
+#         digit_count = 0
+#         sum = 0
+
+#         while num_count > 0:
+#             num_count = num_count // 10
+#             digit_count += 1
+
+#         while num_digit > 0:
+#             digit = num_digit % 10
+#             sum += digit ** digit_count
+#             num_digit = num_digit // 10 
+
+#         if sum == num:
+#             count += 1
+
+#         num += 1  
+                  
+#     return num-1
+
+# print(nth_armstrong(10))
+
+
+def nth_armstrong(n):
     count = 0
     num = 1
-
-    while count != nth:
-        num_count = num
-        num_digit = num
+    while count < n :
+        n_digit = num
+        n_count = num
         digit_count = 0
-        sum = 0
-
-        while num_count > 0:
-            num_count = num_count // 10
+        total = 0
+        while n_count > 0:
+            n_count = n_count // 10
             digit_count += 1
-
-        while num_digit > 0:
-            digit = num_digit % 10
-            sum += digit ** digit_count
-            num_digit = num_digit // 10 
-
-        if sum == num:
+        while n_digit > 0: 
+            total += (n_digit % 10) ** digit_count
+            n_digit = n_digit // 10
+        if num == total:
             count += 1
-
-        num += 1  
-                  
+        if count == n:
+            return num
+        num += 1
     return num-1
 
-print(nth_armstrong(10))
+print(nth_armstrong(10))    
--- basics/Product _of_three_nums.py
+++ basics/Product _of_three_nums.py
@@ -5,15 +5,17 @@ https://codeforces.com/problemset/problem/1294/C
 def product_of_three_nums(n):
     for i in range(2, int(n**0.5) + 1): 
         if n % i == 0:
-            a = i 
             reduced = n // i 
             break
+    else:
+        return None
 
-    for j in range(a+1, int((reduced)**0.5) + 1):
+    for j in range(i+1, int((reduced)**0.5) + 1):
         if reduced % j == 0:
             k = reduced // j
             if k != i and k != j:
                 return i, j, k
+    return None
 
 n = int(input("Enter a number: "))                    
 print(product_of_three_nums(n))
