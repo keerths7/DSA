73fefaa131422d8e0118267cada996e0db2fb7b4
keerths7 <rkeers1111@gmail.com>
2025-02-28 22:06:03 +0530

    added some code

--- LearningDSA/LinkedLists/SLL/delete.py
+++ LearningDSA/LinkedLists/SLL/delete.py
@@ -8,3 +8,47 @@ class LinkedList():
         self.head = None
         self.tail = None
         self.length = 0
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def __str__(self):
+        result = ""
+        temp_node = self.head 
+        while temp_node:
+            result += str(temp_node.value)
+            if temp_node.next:
+                result += " --> "
+            temp_node = temp_node.next
+        return result
+    
+    def delete(self):
+        self.head = None
+        self.tail = None
+        self.length = 0 
+
+linked_list = LinkedList()
+linked_list.append(10)
+linked_list.append(20)
+linked_list.append(30)
+linked_list.append(40)
+print(linked_list)
+print(linked_list.delete())
+print(linked_list)
--- LearningDSA/LinkedLists/SLL/pop.py
+++ LearningDSA/LinkedLists/SLL/pop.py
@@ -55,9 +55,9 @@ class LinkedList:
 
 linked_list = LinkedList()
 linked_list.append(10)
-# linked_list.append(20)
-# linked_list.append(30)
-# linked_list.append(40)
+linked_list.append(20)
+linked_list.append(30)
+linked_list.append(40)
 print(linked_list)
 print(linked_list.pop())
 print(linked_list)
