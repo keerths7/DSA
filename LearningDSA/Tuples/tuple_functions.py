df2a0b910e2373ddf879206f48172069a3934167
keerths7 <rkeers1111@gmail.com>
2025-03-02 19:52:22 +0530

    done with basics

--- LearningDSA/Tuples/tuple_functions.py
+++ LearningDSA/Tuples/tuple_functions.py
@@ -32,31 +32,83 @@ t1 = (3,34,5,6,2)
 print(insert_at_beginning(t1, 6))
 
 
-def tuple_elementwise_sum(): 
+def tuple_elementwise_sum(tuple1, tuple2): 
     if len(tuple1) != len(tuple2):
         raise ValueError("Check the length of the tuples to be equal.")
     sum = 0
     tuple3 = ()
-    for i in tuple1, tuple2:
-        sum = tuple1[i] + tuple[i]
-        tuple3.append(sum)
+    for i in range(len(tuple1)):
+        sum = tuple1[i] + tuple2[i]
+        tuple3 += (sum,)
     return tuple3
 
-
-def tuple_elementwise_sum(t1, t2):
-    if len(t1) != len(t2):
+def tuple_elementwise_sum_zip(tuple1, tuple2):
+    if len(tuple1) != len(tuple2):
         raise ValueError("Input tuples must have the same length.")
-
-    result = tuple(a + b for a, b in zip(t1, t2))
+    result = tuple(a + b for a, b in zip(tuple1, tuple2))
     return result
 
-
-def tuple_elementwise_sum(tuple1, tuple2):
+def tuple_elementwise_sum_zip_map(tuple1, tuple2):
     return tuple(map(sum, zip(tuple1, tuple2)))
 
 t1 = (1, 2, 3)
 t2 = (4, 5, 6)
 print(tuple_elementwise_sum(t1, t2))
+print(tuple_elementwise_sum_zip(t1, t2))
+print(tuple_elementwise_sum_zip_map(t1, t2))
+
+
+def concatenate_strings(input_tuple):
+    output_string = ""
+    for i in input_tuple:
+        output_string = output_string + i + " "
+    return output_string.strip()
+
+def concatenate_strings(input_tuple):
+    return " ".join(input_tuple)
     
+input_tuple = ("hi","hello","bonjour","vanakkam")
+print(concatenate_strings(input_tuple))
 
 
+def diagonal_tuple(matrix_tuple):
+    if len(matrix_tuple) == len(matrix_tuple[0]):
+        return matrix_tuple[0][0], matrix_tuple[1][1], matrix_tuple[2][2]
+    return "diagonal matrix only applies to square matrices"
+
+def diagonal_tuple_loop(matrix_tuple):
+    diagonal_tuple = ()
+    if len(matrix_tuple) == len(matrix_tuple[0]):
+        for i in range(len(matrix_tuple)):
+            diagonal_tuple += (matrix_tuple[i][i],)
+        return diagonal_tuple
+    return "diagonal matrix only applies to square matrices"
+
+def diagonal_tuple_direct(matrix_tuple):
+    if len(matrix_tuple) == len(matrix_tuple[0]):
+        diagonal_tuple = tuple(matrix_tuple[i][i] for i in range(len(matrix_tuple)))
+        return diagonal_tuple
+    return "diagonal matrix only applies to square matrices"
+
+matrix_tuple = ((1,2,3), (4,5,6), (7,8,9))
+print(diagonal_tuple(matrix_tuple))
+print(diagonal_tuple_loop(matrix_tuple))
+print(diagonal_tuple_direct(matrix_tuple))
+
+
+def common_elements(tuple1, tuple2):
+    common_tuple = ()
+    for i in tuple1:
+        for j in tuple2:
+            if i == j:
+                common_tuple += (i, )
+    return common_tuple
+
+def common_elements_set(tuple1, tuple2):
+    return tuple(set(tuple1) & set(tuple2))
+    return tuple(set(tuple1).intersection(set(tuple2)))
+
+tuple1 = (1,2,5,6,7,3)
+tuple2 = (3,5,6,9,4,9,8,7)
+print(common_elements(tuple1, tuple2))
+print(common_elements_set(tuple1, tuple2))
