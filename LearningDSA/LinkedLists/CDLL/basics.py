716e4e3784e2ad36ff3d7328000e81672333ac46
rkeers1111@gmail.com <rkeers1111@gmail.com>
2025-03-26 17:46:38 +0530

    made changes

--- LearningDSA/LinkedLists/CDLL/basics.py
+++ LearningDSA/LinkedLists/CDLL/basics.py
@@ -14,13 +14,5 @@ class CDLinkedList:
     def __init__(self, value):
         new_node = Node(value) 
         self.head = new_node
-        self.tail = new_node
-        new_node.next = new_node
-        new_node.prev = new_node
-        self.length = 1
-
-cdinked_list = CDLinkedList(10)
-print(cdinked_list.head.value)
-print(cdinked_list.tail.value)
-print(cdinked_list.head.next.value)
-print(cdinked_list.head.prev.value)
+        self.tail = new_node    
+        
