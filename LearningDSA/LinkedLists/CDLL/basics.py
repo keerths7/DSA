3403a5d7da79c89dd375b1d9f1785adfb770621b
rkeers1111@gmail.com <rkeers1111@gmail.com>
2025-03-27 00:40:36 +0530

    added some code

--- LearningDSA/LinkedLists/CDLL/basics.py
+++ LearningDSA/LinkedLists/CDLL/basics.py
@@ -15,4 +15,11 @@ class CDLinkedList:
         new_node = Node(value) 
         self.head = new_node
         self.tail = new_node    
-        
+        new_node.next = new_node        # self.head.prev = new_node                                    
+        new_node.prev = new_node        # self.tail.next = new_node
+        self.length = 1
+
+cdlinkedlist = CDLinkedList(10)
+print(cdlinkedlist.head.value)
+print(cdlinkedlist.head.next.value)
+print(cdlinkedlist.tail.next.value)
